ca92daaaf5a28035cc4e6e943bdc9f63cbd4869d
Apelele <kufaapelele@gmail.com>
2025-12-09 15:03:11 +0200

    added javascript

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-alert("Hello World");
+function generatePoem(event) {
+  event.preventDefault();
+
+  new Typewriter(document.querySelector("#poem"), {
+    strings:
+      " In lines of code we often find, A quiet peace within the mind. A world created bit by bit, Where dreams and logic cleanly fit",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
