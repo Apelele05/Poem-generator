e045e9a4673fdaf537ea267a32ef05dcb30b12e9
Apelele <kufaapelele@gmail.com>
2025-12-12 16:10:23 +0200

    added more html

--- src/index.js
+++ src/index.js
@@ -1,13 +1,34 @@
+function displayPoem(response) {
+  console.log("generated poem");
+
+  new Typewriter(document.querySelector("#poem"), {
+    strings: [response.data.answer],
+    autoStart: true,
+    delay: 20,
+    cursor: "",
+  });
+}
+
 function generatePoem(event) {
   event.preventDefault();
 
-  new Typewriter(document.querySelector("#poem"), {
-    strings:
-      " In lines of code we often find, A quiet peace within the mind. A world created bit by bit, Where dreams and logic cleanly fit",
-    autoStart: true,
-    delay: 1,
-    cursor: "",
-  });
+  let instructionsInput = document.querySelector("#user-instructions");
+
+  let apiKey = "fo68et34d0e7d6af2ae13cd3f76c33b0";
+
+  let prompt = `Generate a poem about ${instructionsInput.value}.`;
+  let context =
+    "You are a coding Poem expert and love to write short poems. Your mission is to generate a 4-line poem in basic HTML and separate each line with <br />. Do not include a title. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem.";
+
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${encodeURIComponent(
+    prompt
+  )}&context=${encodeURIComponent(context)}&key=${apiKey}`;
+
+  console.log("generating poem…");
+  console.log("Prompt:", prompt);
+  console.log("Context:", context);
+
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
